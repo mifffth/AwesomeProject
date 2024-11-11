4f9a063a73f67c8cb3aca7028eafac51b7ae591a
mifffth <miftahdesmasyahputra@mail.ugm.ac.id>
2024-11-11 11:23:15 +0700

    adding lists + webview tab in navbar

--- BottomNav.js
+++ BottomNav.js
@@ -2,21 +2,33 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Portofolio from './App'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import Profil from './App';
+import Mahasiswa from './Mahasiswa';
+import { faUser, faInbox } from '@fortawesome/free-solid-svg-icons';
+import { WebView } from 'react-native-webview';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function HomeScreen() {
   return (
-   <Portofolio/>
+   <Profil/>
   );
 }
 
-// function SettingsScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Settings!</Text>
-//     </View>
-//   );
-// }
+function DataMahasiswaScreen() {
+  return (
+    <Mahasiswa/>
+  );
+}
+
+function WebScreen() {
+  return (
+    <WebView
+source={{ uri: 'https://github.com/mifffth' }}
+style={{ marginTop: 20 }}
+/>
+  );
+}
 
 const Tab = createBottomTabNavigator();
 
@@ -24,8 +36,28 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Profile" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Profil" component={HomeScreen} options={{
+          headerShown:false,
+          tabBarIcon: ({color}) => (
+            <FontAwesomeIcon icon={faUser} color={color} size={20}/>
+          ),
+          }}
+          />
+        <Tab.Screen name="Data Mahasiswa" component={DataMahasiswaScreen} options={{
+          headerShown:false,
+          tabBarIcon: ({color}) => (
+            <FontAwesomeIcon icon={faInbox} color={color} size={20}/>
+          ),
+          }}
+          />
+           <Tab.Screen name="GITHub" component={WebScreen} options={{
+          headerShown:false,
+          tabBarIcon: ({color}) => (
+            <FontAwesomeIcon icon={faGithub} color={color} size={20}/>
+          ),
+          }}
+          />
+          
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ Createdata.js
@@ -0,0 +1,112 @@
+import React, {useState} from 'react';
+import { SafeAreaView, View, ScrollView, TextInput, Text, Button, StyleSheet } from 'react-native';
+
+const Createdata = () => {
+  const jsonUrl = 'http://10.0.2.2:3000/mahasiswa';
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
+  const [kelas, setKelas] = useState('');
+  const [gender, setGender] = useState('');
+  const [email, setEmail] = useState('');
+
+  const submit = () => {
+    const data = {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      kelas: kelas,
+      gender: gender,
+    };
+    fetch(jsonUrl, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      alert('Data tersimpan');
+      setFirstName('');
+      setLastName('');
+      setEmail('');
+      setKelas('');
+      setGender('');
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      alert('Failed to save data');
+    });
+  };
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={styles.title}>Tambah Data Mahasiswa</Text>
+        <ScrollView style={styles.form}>
+          <TextInput
+            placeholder="Nama Depan"
+            style={styles.input}
+            value={first_name}
+            onChangeText={(value) => setFirstName(value)}
+          />
+          <TextInput
+            placeholder="Nama Belakang"
+            style={styles.input}
+            value={last_name}
+            onChangeText={(value) => setLastName(value)}
+          />
+          <TextInput
+            placeholder="Kelas"
+            style={styles.input}
+            value={kelas}
+            onChangeText={(value) => setKelas(value)}
+          />
+          <TextInput
+            placeholder="Jenis Kelamin"
+            style={styles.input}
+            value={gender}
+            onChangeText={(value) => setGender(value)}
+          />
+          <TextInput
+            placeholder="Email"
+            style={styles.input}
+            value={email}
+            onChangeText={(value) => setEmail(value)}
+          />
+          <Button title="Simpan" style={styles.button} onPress={submit} />
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Createdata;
+
+const styles = StyleSheet.create({
+  title: {
+    paddingVertical: 12,
+    backgroundColor: '#333',
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  form: {
+    padding: 10,
+    marginBottom: 100,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    borderRadius: 8,
+    padding: 8,
+    width: '100%',
+    marginVertical: 5,
+  },
+  button: {
+    marginVertical: 10,
+  }
+});
